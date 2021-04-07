22a07d480ab4fe0878fbe56f22db7559e8403ed9
Yury Kiselev <agonmountain@outlook.com>
2021-04-07 13:22:03 +0300

    Переосмысление XMLManager

--- engine/xml_module/xml_manager.py
+++ engine/xml_module/xml_manager.py
@@ -1,7 +1,48 @@
 from xml.etree import ElementTree
 import os.path
 
+from data.config import XML_PATH
+
 from engine.xml_module.xml_element import XmlElement
+
+tree = ElementTree.parse(XML_PATH)
+root = tree.getroot()
+
+
+class XmlTreeManager(object):
+    """
+    Работа с деревом xml файла
+    > создание веток
+    > удаление веток
+    > переключение веток
+    >
+    >
+    """
+    def create_branch(self, branch_name):
+        attrib = {'branch_name': branch_name}
+        branch = root.makeelement('branch', attrib)
+        root.append(branch)
+        tree.write(XML_PATH, encoding='UTF-8')
+        return None
+
+    def delete_branch(self, branch_name):
+        attrib = {'branch_name': branch_name}
+        branch = root.find('branch').getiterator(attrib)
+        root.remove(branch)
+        tree.write(XML_PATH, encoding='UTF-8')
+        return None
+
+    def switch_branch(self, branch_name):
+        return None
+
+    def add_element(self, element):
+        return None
+
+
+if __name__ == '__main__':
+    a = XmlTreeManager()
+    a.delete_branch('test_21743')
+
 
 
 class XmlManager(object):
