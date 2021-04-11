b1134d6e55c7242ea6634d5661c55c49f4382e7b
Yury Kiselev <agonmountain@outlook.com>
2021-04-11 23:20:20 +0300

    Переосмысление (XmlManager и XmlElement) и рождение единого XmlTreeManager с функциями: - создать ветку - получить ветку - сменить ветку - удалить ветку - создать элемент - получить элемент - конвертировать элемент - удалить элемент

--- engine/xml_module/xml_manager.py
+++ engine/xml_module/xml_manager.py
@@ -1,65 +1,151 @@
 from xml.etree import ElementTree
-import os.path
-
 from data.config import XML_PATH
-
-from engine.xml_module.xml_element import XmlElement
-
-tree = ElementTree.parse(XML_PATH)
-root = tree.getroot()
 
 
 class XmlTreeManager(object):
     """
-    Работа с деревом xml файла
-    > создание веток
-    > удаление веток
-    > переключение веток
-    >
-    >
+    Работа с ветками и элементами дерева xml файла
+    > получить, создать, удалить ветку
+    > получить из ветки, создать внутри ветки, удалить из ветки элемент
     """
-    def create_branch(self, branch_name):
-        attrib = {'branch_name': branch_name}
-        branch = root.makeelement('branch', attrib)
-        root.append(branch)
-        tree.write(XML_PATH, encoding='UTF-8')
+
+    tree = ElementTree.parse(XML_PATH)
+    root = tree.getroot()
+    branch = None
+
+    def get_branch(self, name):
+        """
+        Получить ветку
+        > name имя ветки
+        > return
+        """
+        branches = self.root.findall('branch')
+        for branch in branches:
+            if str(branch.get('name')) == str(name):
+                return branch
         return None
 
-    def delete_branch(self, branch_name):
-        attrib = {'branch_name': branch_name}
-        branch = root.find('branch').getiterator(attrib)
-        root.remove(branch)
-        tree.write(XML_PATH, encoding='UTF-8')
-        return None
+    def create_branch(self, name):
+        """
+        Создать новую ветку
+        > name уникальное имя новой ветки
+        > return
+        """
+        if (self.get_branch(name) != None):
+            return False
+        else:
+            attrib = {'name': name}
+            branch = self.root.makeelement('branch', attrib)
+            self.root.append(branch)
+            self.tree.write(XML_PATH, encoding='UTF-8')
+            self.switch_branch(name)
+            return True
 
-    def switch_branch(self, branch_name):
-        return None
+    def switch_branch(self, name):
+        """
+        Сменить активную ветку
+        """
+        self.branch = self.get_branch(name)
+        if (self.branch == None):
+            return False
+        else:
+            return True
 
-    def add_element(self, element):
-        return None
+    def delete_branch(self):
+        """
+        Удалить ветку
+        > name уникальное имя существующей ветки
+        > return
+        """
+        if (self.branch == None):
+            return False
+        else:
+            self.root.remove(self.branch)
+            self.branch = None
+            self.tree.write(XML_PATH, encoding='UTF-8')
+            return True
 
-
-if __name__ == '__main__':
-    a = XmlTreeManager()
-    a.delete_branch('test_21743')
-
-
-
-class XmlManager(object):
-    """Работа с xml файлом"""
-
-    def __init__(self, xml_full_file_path, root_name, elements_name):
-        """Конструктор"""
-        self.tree = ElementTree.parse(os.path.abspath(os.path.join(xml_full_file_path)))
-        self.root = self.tree.getroot()
-
-        self.root = self.root.find(root_name)
-        self.elements = self.root.findall(elements_name)
-
-        self.elements = [XmlElement(element) for element in self.elements]
-
-    def get_element_by_id(self, element_id_int):
-        """Получить элемент по id из списка элементов"""
-        for element in self.elements:
-            if str(element.id) == str(element_id_int):
+    def get_element(self, name):
+        """
+        Получить элемент из активной ветки по имени элемента
+        > name имя элемента
+        > return
+        """
+        elements = self.branch.findall('element')
+        for element in elements:
+            if str(element.get('name')) == str(name):
                 return element
+        return None
+
+    def create_element(self, name, txt, adds, trs):
+        """
+        Создать элемент внутри ветки
+        > branch ветка
+        > name имя элемента
+        > txt текст элемента
+        > adds дополнения в виде списка словарей ([{'branch_name':'*имя ветки*', 'element_name':'*имя элемента*'}, ...])
+        > trs переходы в виде списка словарей ([{'branch_name':'*имя ветки*', 'next_element_name':'*имя элемента*'}, ...])
+        > return
+        """
+        attrib = {'name': name}
+        element = ElementTree.SubElement(self.branch, 'element', attrib)
+
+        text = ElementTree.SubElement(element, 'text')
+        text.text = txt
+
+        additions = ElementTree.SubElement(element, 'additions')
+        transitions = ElementTree.SubElement(element, 'transitions')
+        # создание списка additions [addition , addition , ...]
+        for add in adds:
+            attrib = {'branch_name': add['branch_name'], 'element_name': add['element_name']}
+            addition = ElementTree.SubElement(additions, 'addition', attrib)
+            addition.text = add['text']
+        # создание списка transitions [transition , transition , ...]
+        for tr in trs:
+            attrib = {'branch_name': tr['branch_name'], 'next_element_name': tr['next_element_name']}
+            transition = ElementTree.SubElement(transitions, 'transition', attrib)
+            transition.text = tr['text']
+
+        self.tree.write(XML_PATH, encoding='UTF-8')
+
+    def delete_element(self, element):
+        """
+        Удалить элемент из активной ветки
+        > element элемент
+        > return
+        """
+        if (self.branch != None):
+            self.branch.remove(element)
+            self.tree.write(XML_PATH, encoding='UTF-8')
+            return True
+        else:
+            return False
+
+    def convert(self, element):
+        """
+        Конвертировать element в удобный для использования формат XmlElement
+        > element элемент для конвертации
+        > return
+        """
+        return XmlElement(element)
+
+
+class XmlElement(object):
+    """
+    Элемент ветки дерева xml файла
+    """
+
+    def __init__(self, element):
+        """
+        Конструктор
+        """
+        self.element = element
+
+        self.id = self.element.get('name')
+        self.text = self.element.findall('text')[0]
+
+        self.additions = self.element.findall('additions')[0]
+        self.transitions = self.element.findall('transitions')[0]
+
+        self.addition_list = self.additions.findall('addition')
+        self.transition_list = self.transitions.findall('transition')
